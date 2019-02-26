8704f2def74a716ef273fd3a9aae1255a453d68a
Luke <lukebbarrier@gmail.com>
2019-02-25 21:00:44 -0500

    f

--- client/app.js
+++ client/app.js
@@ -10,7 +10,10 @@ function setup() {
   noCanvas();
   scene = document.getElementById("main");
   s.on("connect", (d) => {
-    var box = document.createElement("a-box").setAttribute("position", "0 1 -5").setAttribute("color", "#f46842").setAttribute("id", `${d}`);
+    var box = document.createElement("a-box");
+    box.setAttribute("position", "0 1 -5");
+    box.setAttribute("color", "#f46842");
+    box.setAttribute("id", `${d}`);
     scene.appendChild(box);
   });
 }
