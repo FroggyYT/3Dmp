779d5fddc625b96eaecef1f3cf5d33a82c5ce2ba
Luke <lukebbarrier@gmail.com>
2019-02-25 20:34:51 -0500

    Make everything better with P5

--- client/app.js
+++ client/app.js
@@ -1,7 +1,14 @@
-setTimeout(() => {
-  var scene = document.getElementById("main")
-  var s = io();
+var s;
+
+function preload() {
+  s = io;
+}
+
+var scene;
+
+function setup() {
+  scene = document.getElementById("main")
   s.on("connect", (d) => {
     scene.innerHTML += `<a-box position="-5 1 0" color="#FFFFF" id="${d.id}"></a-box>`;
   });
-},1);
+}
