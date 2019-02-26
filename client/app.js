48610b50cb30405e5e066c8224e9299883ad1721
Luke <lukebbarrier@gmail.com>
2019-02-25 20:51:14 -0500

    f

--- client/app.js
+++ client/app.js
@@ -10,6 +10,6 @@ function setup() {
   noCanvas();
   scene = document.getElementById("main");
   s.on("connect", (d) => {
-    scene.innerHTML += `<a-box position="-5 1 0" color="#f46842" id="${d}"></a-box>`;
+    // scene.innerHTML += `<a-box position="-5 1 0" color="#f46842" id="${d}"></a-box>`;
   });
 }
