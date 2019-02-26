89a47526f994539ca5f507d8999a65ef9aa16569
Luke <lukebbarrier@gmail.com>
2019-02-25 20:27:21 -0500

    Heroku Error Fixes

--- client/app.js
+++ client/app.js
@@ -1,7 +1,7 @@
-var s = io();
-
-setTimeout(() => {var scene = document.getElementById("main")},1);
-
-s.on("connect", (d) => {
-  scene.innerHTML += `<a-box position="-5 1 0" color="#FFFFF" id="${d.id}"></a-box>`;
-});
+setTimeout(() => {
+  var scene = document.getElementById("main")
+  var s = io();
+  s.on("connect", (d) => {
+    scene.innerHTML += `<a-box position="-5 1 0" color="#FFFFF" id="${d.id}"></a-box>`;
+  });
+},1);
