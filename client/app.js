d73b7d0ec91f6506a3e37924e121966ca6d4550e
Luke <lukebbarrier@gmail.com>
2019-03-02 10:17:43 -0500

    f

--- client/app.js
+++ client/app.js
@@ -19,7 +19,8 @@ function setup() {
   if (/*window.mobilecheck()*/true) {
     document.addEventListener("touchstart", () => {
       var directions = document.getElementById("camera").components.camera.camera.getWorldDirection().multiplyScalar(0.5);
-      document.getElementById("camera").components.camera.camera.parent.position.add({x:directions.x, y:0, z:directions.z});
+      var newP = document.getElementById("camera").components.camera.camera.parent.position.add({x:directions.x, y:0, z:directions.z});
+      document.getElementById("camera").setAttribute("position", newP);
     });
   }
 
